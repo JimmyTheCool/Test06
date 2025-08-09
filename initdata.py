f5decdcae8e5ea0e386cb2a05185cc70c45508d4
James Shaw <Jamesshaw@live.com.au>
2025-08-09 17:45:59 +0800

    20250809 - JamesBond2

--- initdata.py
+++ initdata.py
@@ -4,12 +4,14 @@
 bob = {'name': 'Bob Smith', 'age': 42, 'pay': 30000, 'job': 'dev'}
 sue = {'name': 'Sue Jones', 'age': 45, 'pay': 40000, 'job': 'hdw'}
 tom = {'name': 'Tom', 'age': 50, 'pay': 0, 'job': None}
+James = {'name': 'James Bond2', 'age': 35, 'pay': 50000, 'job': 'spy'}
 
 # database
 db = {}
 db['bob'] = bob
 db['sue'] = sue
 db['tom'] = tom
+db['james'] = James
 
 if __name__ == '__main__': # when run as a script
     for key in db:
